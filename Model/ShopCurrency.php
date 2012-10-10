22deff56d0e93c6779b4b04669ffba3216de78ab
dogmatic69 <dogmatic69@gmail.com>
2012-10-10 03:15:13 +0100

    adding code for getting currency data

--- Model/ShopCurrency.php
+++ Model/ShopCurrency.php
@@ -6,16 +6,10 @@ App::uses('ShopAppModel', 'Shop.Model');
  * @property ShopPaymentMethodApi $ShopPaymentMethodApi
  */
 class ShopCurrency extends ShopAppModel {
-
-/**
- * Display field
- *
- * @var string
- */
-	public $displayField = 'name';
-
-
-	//The Associations below have been created with all possible keys, those that are not needed can be removed
+	public $findMethods = array(
+		'currency' => true,
+		'conversions' => true
+	);
 
 /**
  * hasMany associations
@@ -37,5 +31,84 @@ class ShopCurrency extends ShopAppModel {
 			'counterQuery' => ''
 		)
 	);
+
+/**
+ * @brief get a currency
+ *
+ * @code
+ *	// Pass currency for specific currency, else default is used.
+ *	$this->ShopCurrency->find('currency', array('currency' => 'usd'));
+ *  return array(
+ *		... // usd currency
+ *  );
+ *
+ *	// get the stores default (the one with a factor of 1
+ *	// If two currencies are the same, non defaults should be set to .99999 or 1.00001
+ *	$this->ShopCurrency->find('currency');
+ *  return array(
+ *		... // default currency
+ *  );
+ * @endcode
+ *
+ * @param string $state
+ * @param array $query
+ * @param array $results
+ */
+	public function _findCurrency($state, array $query, array $results = array()) {
+		if($state == 'before') {
+			if(!empty($query['currency'])) {
+				$query['conditions'] = array(
+					$this->alias . '.code' => $query['currency']
+				);
+			} else {
+				$query['conditions'] = array(
+					$this->alias . '.factor' => 1,
+				);
+			}
+
+			$fields = $this->schema();
+			unset($fields[$this->primaryKey], $fields['created'], $fields['modified']);
+			$query['fields'] = array_keys($fields);
+
+			$query['limit'] = 1;
+
+			return $query;
+		}
+
+		if(empty($results)) {
+			throw new CakeException(__d('shop', 'Selected currency was not found'));
+		}
+
+		return current($results);
+	}
+
+/**
+ * @brief get a list of the factors for conversions
+ *
+ * return a key value array of currency code - factor.
+ *
+ * @param array $state
+ * @param array $query
+ * @param array $results
+ *
+ * @return array
+ */
+	public function _findConversions($state, array $query, array $results = array()) {
+		if($state == 'before') {
+			if(empty($query['fields'])) {
+				$query['fields'] = array(
+					$this->alias . '.code',
+					$this->alias . '.factor'
+				);
+			}
+
+			return $query;
+		}
+
+		return Hash::combine($results,
+			sprintf('{n}.%s.code', $this->alias),
+			sprintf('{n}.%s.factor', $this->alias)
+		);
+	}
 
 }
--- Test/Case/Model/ShopCurrencyTest.php
+++ Test/Case/Model/ShopCurrencyTest.php
@@ -39,8 +39,203 @@ class ShopCurrencyTest extends CakeTestCase {
 		parent::tearDown();
 	}
 
-	public function testSomething() {
-		
+/**
+ * @brief check there are exceptions thrown when there is no currencies available
+ *
+ * @expectedException CakeException
+ */
+	public function testFindCurrencyNoneAvailable() {
+		$this->assertTrue((bool)$this->{$this->modelClass}->deleteAll(array('ShopCurrency.id != ' => null)));
+		$this->{$this->modelClass}->find('currency');
+	}
+
+/**
+ * @brief Check that you can set a currency to near 1 without it becoming default
+ *
+ * @expectedException CakeException
+ *
+ * @dataProvider nearOneIsNotOneDataProvider
+ */
+	public function testNearOneIsNotOne($data) {
+		$this->{$this->modelClass}->id = 'gbp';
+		$this->assertTrue((bool)$this->{$this->modelClass}->saveField('factor', $data));
+		$this->{$this->modelClass}->find('currency');
+	}
+
+/**
+ * near one is not one data provider
+ * @return type
+ */
+	public function nearOneIsNotOneDataProvider() {
+		return array(
+			array('1.0000001'),
+			array('0.9999999')
+		);
+	}
+
+/**
+ * @brief test finding the default + when it changes
+ */
+	public function testFindCurrencyDefault() {
+		$expected = array(
+			'ShopCurrency' => array(
+				'name' => 'gbp',
+				'code' => 'gbp',
+				'factor' => 1,
+				'whole_symbol' => '£',
+				'whole_position' => 0,
+				'fraction_symbol' => 'p',
+				'fraction_position' => 1,
+				'zero' => '0',
+				'places' => 2,
+				'thousands' => ',',
+				'decimals' => '.',
+				'negative' => '-',
+				'escape' => 1,
+			)
+		);
+		$results = $this->{$this->modelClass}->find('currency');
+		$this->assertEquals($expected, $results);
+
+		$this->{$this->modelClass}->id = 'gbp';
+		$this->assertTrue((bool)$this->{$this->modelClass}->saveField('factor', '1.2'));
+
+		$this->{$this->modelClass}->id = 'usd';
+		$this->assertTrue((bool)$this->{$this->modelClass}->saveField('factor', '1'));
+		$expected = array(
+			'ShopCurrency' => array(
+				'name' => 'usd',
+				'code' => 'usd',
+				'factor' => '1.00000000',
+				'whole_symbol' => '$',
+				'whole_position' => false,
+				'fraction_symbol' => 'c',
+				'fraction_position' => true,
+				'zero' => '-',
+				'places' => 2,
+				'thousands' => ',',
+				'decimals' => '.',
+				'negative' => '-',
+				'escape' => 1,
+			)
+		);
+		$results = $this->{$this->modelClass}->find('currency');
+		$this->assertEquals($expected, $results);
+	}
+
+/**
+ * @brief test find currency specific
+ *
+ * @dataProvider findCurrencySpecificDataProvider
+ */
+	public function testFindCurrencySpecific($data, $expected) {
+		if(!empty($expected)) {
+			$expected = array($this->modelClass => $expected);
+		}
+
+		$result = $this->{$this->modelClass}->find('currency', array('currency' => $data));
+
+		if($result) {
+			$res = round($result[$this->modelClass]['factor'], 4);
+			$exp = round($expected[$this->modelClass]['factor'], 4);
+			$this->assertEquals($exp, $res);
+			unset($result[$this->modelClass]['factor'], $expected[$this->modelClass]['factor']);
+		}
+		$this->assertEquals($expected, $result);
+	}
+
+/**
+ * @brief find currency specific data provider
+ *
+ * @return array
+ */
+	public function findCurrencySpecificDataProvider() {
+		return array(
+			array(
+				false,
+				array(
+					'name' => 'gbp',
+					'code' => 'gbp',
+					'factor' => 1,
+					'whole_symbol' => '£',
+					'whole_position' => 0,
+					'fraction_symbol' => 'p',
+					'fraction_position' => 1,
+					'zero' => '0',
+					'places' => 2,
+					'thousands' => ',',
+					'decimals' => '.',
+					'negative' => '-',
+					'escape' => 1,
+				)
+			),
+			array(
+				'gbp',
+				array(
+					'name' => 'gbp',
+					'code' => 'gbp',
+					'factor' => 1,
+					'whole_symbol' => '£',
+					'whole_position' => 0,
+					'fraction_symbol' => 'p',
+					'fraction_position' => 1,
+					'zero' => '0',
+					'places' => 2,
+					'thousands' => ',',
+					'decimals' => '.',
+					'negative' => '-',
+					'escape' => 1,
+				)
+			),
+			array(
+				'usd',
+				array(
+					'name' => 'usd',
+					'code' => 'usd',
+					'factor' => '1.5999',
+					'whole_symbol' => '$',
+					'whole_position' => 0,
+					'fraction_symbol' => 'c',
+					'fraction_position' => 1,
+					'zero' => '-',
+					'places' => 2,
+					'thousands' => ',',
+					'decimals' => '.',
+					'negative' => '-',
+					'escape' => 1,
+				)
+			)
+		);
+	}
+
+/**
+ * @brief test find conversion data
+ */
+	public function testFindConversionsData() {
+		$round = function($data) {
+			array_walk($data, function(&$res) {
+				$res = round($res, 4);
+			});
+
+			return $data;
+		};
+
+		$expected = array(
+			'gbp' => '1.0000',
+			'usd' => '1.5999',
+			'eur' => '1.2425'
+		);
+		$result = $round($this->{$this->modelClass}->find('conversions'));
+
+		$this->assertEquals($expected, $result);
+
+		$this->assertTrue($this->{$this->modelClass}->deleteAll(array($this->modelClass . '.id' => 'eur')));
+		$expected = array(
+			'gbp' => '1.0000',
+			'usd' => '1.5999'
+		);
+		$result = $round($this->{$this->modelClass}->find('conversions'));
+		$this->assertEquals($expected, $result);
 	}
 
 }
