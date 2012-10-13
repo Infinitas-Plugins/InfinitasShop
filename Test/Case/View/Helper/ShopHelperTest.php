6870ee9d1cf61bf72574e1acf032dadabaae4fef
dogmatic69 <dogmatic69@gmail.com>
2012-10-13 01:28:42 +0100

    adding tests for the helper methods

---
+++ Test/Case/View/Helper/ShopHelperTest.php
@@ -0,0 +1,148 @@
+<?php
+App::uses('View', 'View');
+App::uses('Helper', 'View');
+App::uses('ShopHelper', 'Shop.View/Helper');
+
+/**
+ * ShopHelper Test Case
+ *
+ */
+class ShopHelperTest extends CakeTestCase {
+	public $fixtures = array(
+		'plugin.routes.route',
+		'plugin.themes.theme'
+	);
+/**
+ * setUp method
+ *
+ * @return void
+ */
+	public function setUp() {
+		parent::setUp();
+		$View = new View();
+		$this->Shop = new ShopHelper($View);
+	}
+
+/**
+ * tearDown method
+ *
+ * @return void
+ */
+	public function tearDown() {
+		unset($this->Shop);
+
+		parent::tearDown();
+	}
+
+/**
+ * testEmailLink method
+ *
+ * @dataProvider emailLinkDataProvider
+ */
+	public function testEmailLink($data, $expected) {
+		$result = $this->Shop->emailLink($data['email'], $data['icon'], $data['config']);
+		$this->assertTags($result, $expected);
+	}
+
+/**
+ * @brief email link data provider
+ * 
+ * @return array
+ */
+	public function emailLinkDataProvider() {
+		return array(
+			'icon' => array(
+				array(
+					'email' => 'bob@bob.com',
+					'icon' => true,
+					'config' => array()
+				),
+				array(
+					array('a' => array('href' => 'mailto:bob@bob.com', 'title' => 'bob@bob.com', 'target' => '_blank')),
+					array('img' => array('src' => '/emails/img/icon.png', 'width' => 24, 'alt' => 'Email')),
+					'/a'
+				)
+			),
+			'no-icon' => array(
+				array(
+					'email' => 'bob@bob.com',
+					'icon' => false,
+					'config' => array()
+				),
+				array(
+					array('a' => array('href' => 'mailto:bob@bob.com', 'title' => 'bob@bob.com', 'target' => '_blank')),
+					'bob@bob.com',
+					'/a'
+				)
+			),
+			'config' => array(
+				array(
+					'email' => 'bob@bob.com',
+					'icon' => true,
+					'config' => array(
+						'target' => '_self',
+						'title' => 'Awesome title'
+					)
+				),
+				array(
+					array('a' => array('href' => 'mailto:bob@bob.com', 'title' => 'Awesome title', 'target' => '_self')),
+					array('img' => array('src' => '/emails/img/icon.png', 'width' => 24, 'alt' => 'Email')),
+					'/a'
+				)
+			)
+		);
+	}
+
+/**
+ * @brief test time estimate
+ *
+ * @dataProvider timeEstimateDataProvider
+ */
+	public function testTimeEstimate($data, $expected) {
+		$result = $this->Shop->timeEstimate($data);
+		$this->assertEquals($expected, $result);
+	}
+
+/**
+ * @brief time estimate data porvider
+ * 
+ * @return array
+ */
+	public function timeEstimateDataProvider() {
+		return array(
+			'hours-1' => array(1, '1 hour'),
+			'hours-20' => array(20, '20 hours'),
+			'hours-25' => array(25, '1 day'),
+			'hours-100' => array(100, '4 days'),
+			'hours-200' => array(200, '1 week'),
+			'hours-400' => array(400, '2 weeks'),
+			'hours-1000' => array(1000, '6 weeks'),
+			'hours-1500' => array(1500, '2 months'),
+		);
+	}
+
+/**
+ * @brief test admin currency
+ *
+ * @dataProvider adminCurrencyDataProvider
+ */
+	public function testAdminCurrency($data, $expected) {
+		$result = $this->Shop->adminCurrency($data);
+		$this->assertEquals($expected, $result);
+	}
+
+/**
+ * @brief admin currency data provider
+ * 
+ * @return array
+ */
+	public function adminCurrencyDataProvider() {
+		return array(
+			'penny' => array(.1, '10p'),
+			'pound' => array(1, '&#163;1.00'),
+			'high' => array(1234.56, '&#163;1,234.56'),
+			'neg' => array(-123.999, '(&#163;124.00)')
+		);
+	}
+
+}
--- View/Helper/ShopHelper.php
+++ View/Helper/ShopHelper.php
@@ -5,16 +5,35 @@ class ShopHelper extends AppHelper {
 		'Html'
 	);
 
+/**
+ * @brief link email addresses with optional icon 
+ *
+ * Can link as plain text or as an icon
+ *
+ * @code 
+ * 	// using an icon
+ * 	$this->Shop->emailLink('foo@bar.com'); // <a ...><img ... /></a>
+ *
+ * 	// using text
+ * 	$this->Shop->emailLink('foo@bar.com', false); // <a ...>foo@bar.com</a>
+ * @endcode
+ * 
+ * @param string $email the email address being linked
+ * @param boolean $icon use an icon or not
+ * @param array $options options for the icon link
+ * 
+ * @return sting
+ */
 	public function emailLink($email, $icon = true, $options = array()) {
-		if(!$icon) {
-			return $this->Text->autoLinkEmails($email);
-		}
-
-		$options = array_merge($options, array(
+		$options = array_merge(array(
 			'escape' => false,
 			'title' => $email,
-			'target' => 'blank'
-		));
+			'target' => '_blank'
+		), $options);
+
+		if(!$icon) {
+			return $this->Text->autoLinkEmails($email, $options);
+		}
 
 		return $this->Html->link(
 			$this->Html->image('Emails.icon.png', array(
@@ -26,20 +45,46 @@ class ShopHelper extends AppHelper {
 		);
 	}
 
+/**
+ * @brief convert hours to meaningful time
+ *
+ * @code 
+ * 	$this->Shop->timeEstimate(12); // 12 hours
+ * 	$this->Shop->timeEstimate(25); // 1 day
+ * 	$this->Shop->timeEstimate(360); // 2 weeks
+ * @endcode
+ * 
+ * @param integer $hours the number of hours being converted
+ * 
+ * @return string
+ */
 	public function timeEstimate($hours) {
+		$hours = round($hours);
 		if($hours <= 24) {
-			return __d('shop', '%d hours', round($hours));
+			return __dn('shop', '%d hour', '%d hours', $hours, $hours);
 		}
 
 		$days = round($hours / 24);
 		if($days <= 7) {
-			return __d('shop', '%d days', $days);			
+			return __dn('shop', '%d day', '%d days', $days, $days);
 		}
 
-		return __d('shop', '%d weeks', round($days / 7));
+		$weeks = round($days / 7);
+		if($weeks <= 6) {
+			return __dn('shop', '%d week', '%d weeks', $weeks, $weeks);	
+		}
 
+		$months = round($weeks / 4.2);
+		return __dn('shop', '%d month', '%d months', $months, $months);	
 	}
 
+/**
+ * @brief convert an amount to currency
+ * 
+ * @param float $amount the amount to show
+ * 
+ * @return string
+ */
 	public function adminCurrency($amount) {
 		return CakeNumber::currency($amount, 'GBP');
 	}
