4071513b808e833970e52869c1817c60bd462b36
dogmatic69 <dogmatic69@gmail.com>
2012-10-25 15:01:43 +0100

    adding SEO config for the webmaster plugin

--- Config/config.php
+++ Config/config.php
@@ -1,14 +1,29 @@
 <?php
-	$config['Shop'] = array(
-		'currency' => 'GBP',
-		'display_category_count' => true,
+$config['Shop'] = array(
+	'currency' => 'GBP',
+	'display_category_count' => true,
+	'robots' => array(
+		'index' => array(
+			'index' => true,
+			'follow' => true
+		),
+		'view' => array(
+			'index' => true,
+			'follow' => true
+		),
+	),
+	'meta' => array(
+		'keywords' => 'infinitas shop, infinitas ecommerce, cakephp shop, cakephp ecommerce',
+		'description' => 'Infinitas Shop is a powerful ecommerce plugin built on top of the Infinitas CMS. For more information see http://infinitas-cms.org',
+		'title' => 'Infinitas Shop'
+	),
 
-		'allow_ratings' => true,
-		'allow_comments' => true,
-		'payment_terms' => array(
-			__d('shop', 'Cash')    => __d('shop', 'Cash'),
-			__d('shop', '30 days') => __d('shop', '30 days'),
-			__d('shop', '60 days') => __d('shop', '60 days'),
-			__d('shop', '90 days') => __d('shop', '90 days'),
-		)
-	);
+	'allow_ratings' => true,
+	'allow_comments' => true,
+	'payment_terms' => array(
+		__d('shop', 'Cash')    => __d('shop', 'Cash'),
+		__d('shop', '30 days') => __d('shop', '30 days'),
+		__d('shop', '60 days') => __d('shop', '60 days'),
+		__d('shop', '90 days') => __d('shop', '90 days'),
+	)
+);
