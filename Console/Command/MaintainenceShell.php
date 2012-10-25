0ae4fff15c315c516c943fd78049aa93254deefe
dogmatic69 <dogmatic69@gmail.com>
2012-10-25 02:58:14 +0100

    adding basic method for clearing product data

---
+++ Console/Command/MaintainenceShell.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * @brief maintainence shell for the shop plugin
+ *
+ * @property ShopProduct $ShopProduct
+ */
+class MaintainenceShell extends AppShell {
+	public $uses = array(
+		'Shop.ShopProduct'
+	);
+
+	public function main() {
+
+	}
+
+/**
+ * @brief delete all products and relations to the product
+ *
+ * @throws Exception
+ */
+	public function clear_products() {
+		try {
+			$ids = $this->ShopProduct->find('list', array(
+				'fields' => array(
+					$this->ShopProduct->alias . '.' . $this->ShopProduct->primaryKey,
+					$this->ShopProduct->alias . '.shop_image_id'
+				),
+				'contain' => false
+			));
+			$this->ShopProduct->unbindModel(array(
+				'hasOne' => array('ShopCurrentSpecial')
+			), false);
+
+			$Folder = new Folder();
+			foreach(array_keys($ids) as $id) {
+				$this->ShopProduct->delete($id);
+				$this->ShopProduct->ShopImage->delete($ids[$id]);
+
+				$folder = APP . 'webroot' . DS . 'files' . DS . 'shop_image' . DS . 'image' . DS . $ids[$id];
+				$Folder->delete($folder);
+			}
+		} catch(Exception $e) {
+			$this->quit($e->getMessage());
+			throw $e;
+		}
+
+		$this->quit('All products removed');
+	}
+
+}
