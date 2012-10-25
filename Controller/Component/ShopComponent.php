e5a83648e02d31d39857575228a0aec1c3fc8024
dogmatic69 <dogmatic69@gmail.com>
2012-10-25 02:49:06 +0100

    include some data for all pages such as categories for navigation and available currencies

--- Controller/Component/ShopComponent.php
+++ Controller/Component/ShopComponent.php
@@ -1,6 +1,34 @@
 <?php
 App::uses('InfinitasComponent', 'Libs.Controller/Component');
+App::uses('ShopCurrencyLib', 'Shop.Lib');
 
 class ShopComponent extends InfinitasComponent {
+/**
+ * @brief load up required variables for the store
+ *
+ * @param Controller $Controller the controller being run
+ *
+ * @return boolean
+ */
+	public function beforeRender(Controller $Controller) {
+		$shopCurrencies = ClassRegistry::init('Shop.ShopCurrency')->find('switch');
+		$shopCategoriesNav = ClassRegistry::init('Shop.ShopCategory')->find('threaded', array(
+			'fields' => array(
+				'ShopCategory.id',
+				'ShopCategory.name',
+				'ShopCategory.slug',
+				'ShopCategory.parent_id',
+				'ShopCategory.lft',
+				'ShopCategory.rght',
+				'ShopCategory.shop_product_count'
+			),
+			'conditions' => array(
+				'ShopCategory.active' => 1
+			)
+		));
+		$shopBrandsList = ClassRegistry::init('Shop.ShopBrand')->find('brands');
+		$Controller->set(compact('shopCurrencies', 'shopCategoriesNav', 'shopBrandsList'));
 
+		return parent::beforeRender($Controller);
+	}
 }
