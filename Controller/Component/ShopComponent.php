f8b91f001f1ca536896961a06e33547dd7f933cd
dogmatic69 <dogmatic69@gmail.com>
2012-11-03 22:56:36 +0000

    adding calls to fetch data for modules

--- Controller/Component/ShopComponent.php
+++ Controller/Component/ShopComponent.php
@@ -11,6 +11,10 @@ class ShopComponent extends InfinitasComponent {
  * @return boolean
  */
 	public function beforeRender(Controller $Controller) {
+		if(isset($Controller->request->params['admin']) && $Controller->request->params['admin']) {
+			return parent::beforeRender($Controller);
+		}
+
 		$shopCurrencies = ClassRegistry::init('Shop.ShopCurrency')->find('switch');
 		$shopCategoriesNav = ClassRegistry::init('Shop.ShopCategory')->find('threaded', array(
 			'fields' => array(
@@ -29,6 +33,26 @@ class ShopComponent extends InfinitasComponent {
 		$shopBrandsList = ClassRegistry::init('Shop.ShopBrand')->find('brands');
 		$Controller->set(compact('shopCurrencies', 'shopCategoriesNav', 'shopBrandsList'));
 
+		$this->_moduleData($Controller);
+
 		return parent::beforeRender($Controller);
+	}
+
+	protected function _moduleData(Controller $Controller) {
+		$ShopProduct = ClassRegistry::init('Shop.ShopProduct');
+		
+		$shopRecentlyViewed = $ShopProduct->find('recentlyViewed', array(
+			'limit' => 5
+		));
+		$shopNewProducts = $ShopProduct->find('new', array(
+			'limit' => 5
+		));
+		$shopPopularProducts = $ShopProduct->find('mostViewed', array(
+			'limit' => 5
+		));
+
+		//$shopRelatedCategories = $ShopProduct->ShopCategoriesProduct->ShopCategory->find('all');
+
+		$Controller->set(compact('shopRecentlyViewed', 'shopNewProducts', 'shopPopularProducts', 'shopRelatedCategories'));
 	}
 }
