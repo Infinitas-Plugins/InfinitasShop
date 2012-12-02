9d3335713c4fb4934988c19f828a6281360598ce
dogmatic69 <dogmatic69@gmail.com>
2012-12-02 23:33:59 +0000

    adding dashboard page for shop frontend

--- Controller/ShopController.php
+++ Controller/ShopController.php
@@ -1,10 +1,15 @@
 <?php
 class ShopController extends ShopAppController {
+
+	public function dashboard() {
+
+	}
+	
 	public function admin_dashboard() {
 
 	}
 
 	public function admin_configuration() {
-		
+
 	}
 }
