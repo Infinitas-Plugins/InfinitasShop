5b7ec08986993b540006e132c3ba4f821347cf5c
dogmatic69 <dogmatic69@gmail.com>
2013-02-14 11:12:31 +0000

    removing the connected method

--- Model/ShopAttribute.php
+++ Model/ShopAttribute.php
@@ -15,7 +15,6 @@
 class ShopAttribute extends ShopAppModel {
 
 	public $findMethods = array(
-		'connected' => true,
 		'productAttributes' => true
 	);
 
@@ -142,116 +141,5 @@ class ShopAttribute extends ShopAppModel {
 		}
 
 		return (array)Hash::extract($results, '{n}.' . $this->alias);
-	}
-
-	protected function _findConnected($state, array $query, array $results = array()) {
-		if ($state == 'before') {
-			$query = array_merge(array(
-				'category' => null,
-				'product' => null
-			), $query);
-
-			$query['joins'] = (array)$query['joins'];
-			$query['joins'][] = $this->autoJoinModel(array(
-				'model' => $this->ShopProductAttribute,
-				'type' => 'right'
-			));
-			$productJoin = $this->ShopProductAttribute->autoJoinModel(array(
-				'model' => $this->ShopProductAttribute->ShopProduct
-			));
-			if (!empty($query['product'])) {
-				$productJoin['conditions']['or'] = array(
-					$this->ShopProductAttribute->ShopProduct->alias . '.' . $this->ShopProductAttribute->ShopProduct->primaryKey => $query['product'],
-					$this->ShopProductAttribute->ShopProduct->alias . '.slug' => $query['product'],
-				);
-			}
-			$query['joins'][] = $productJoin;
-
-			if (!empty($query['category'])) {
-				$query['joins'][] = $this->ShopProductAttribute->ShopProduct->autoJoinModel(array(
-					'model' => $this->ShopProductAttribute->ShopProduct->ShopCategoriesProduct,
-					'type' => 'right',
-				));
-				$categoryJoin = $this->ShopProductAttribute->ShopProduct->ShopCategoriesProduct->autoJoinModel(array(
-					'model' => $this->ShopProductAttribute->ShopProduct->ShopCategoriesProduct->ShopCategory,
-					'conditions' => array(
-						'ShopCategoriesProduct.shop_category_id = ShopCategory.id',
-						'ShopCategory.active = 1'
-					)
-				));
-
-				$query['joins'][] = $categoryJoin;
-				$query['joins'][] = $this->autoJoinModel($this->ShopAttributeGroup);
-			}
-
-			$query['conditions'] = array_merge((array)$query['conditions'], array(
-				//'ShopCategory.slug' => $query['category']
-			));
-
-			$query['group'] = array(
-				$this->alias . '.' . $this->primaryKey
-			);
-			return $query;
-		}
-
-		if (empty($results)) {
-			return array();
-		}
-
-		$results = Hash::extract($results, '{n}.' . $this->alias);
-		$attributeGroups = $this->ShopAttributeGroup->find('all', array(
-			'conditions' => array(
-				$this->ShopAttributeGroup->alias . '.' . $this->ShopAttributeGroup->primaryKey => Hash::extract($results, '{n}.shop_attribute_group_id')
-			)
-		));
-
-		foreach ($attributeGroups as &$group) {
-			$extractTemplate = sprintf('{n}[shop_attribute_group_id=%s]', $group[$this->ShopAttributeGroup->alias][$this->ShopAttributeGroup->primaryKey]);
-			$group[$this->alias] = Hash::extract($results, $extractTemplate);
-		}
-		$colourAttributes = array(
-			'ShopAttributeGroup' => array(
-				'name' => __d('shop', 'Product Colour'),
-				'slug' => 'product-colour',
-				'_generated' => true
-			),
-		);
-
-		$ShopImage = ClassRegistry::init('Shop.ShopImage');
-		$ShopImage->virtualFields['name'] = 'ShopOptionValue.name';
-		$ShopImage->virtualFields['id'] = 'ShopImage.colour_1';
-		$ShopImage->virtualFields['slug'] = 'ShopImage.colour_1';
-		$ShopImage->virtualFields['shop_product_attribute_count'] = 'SUM(ShopImage.id)';
-		$colourAttributes['ShopAttribute'] = $ShopImage->find('all', array(
-			'fields' => array(
-				'ShopImage.id',
-				'ShopImage.name',
-				'ShopImage.slug',
-				'ShopImage.shop_product_attribute_count'
-			),
-			'conditions' => array(
-				'ShopImage.colour_1 !=' => null
-			),
-			'joins' => array(
-				array(
-					'alias' => 'ShopOptionValue',
-					'table' => 'shop_option_values',
-					'type' => 'right',
-					'conditions' => array(
-						'ShopOptionValue.colour = ShopImage.colour_1'
-					)
-				)
-			),
-			'group' => array(
-				'ShopOptionValue.colour'
-			),
-			'order' => array(
-				'ShopImage.colour_1'
-			),
-			'limit' => 20
-		));
-		$colourAttributes['ShopAttribute'] = Hash::extract($colourAttributes['ShopAttribute'], '{n}.ShopImage');
-		$attributeGroups[] = $colourAttributes;
-		return $attributeGroups;
 	}
 }
